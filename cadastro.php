c1c5f4b93f50af1a2c460c96622f4dcc84d4d1fc
Joao Paulo <jp1536@hotmail.com>
2019-05-16 18:11:11 -0300

    Telas de login e cadastro.

---
+++ cadastro.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="pt" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta id="viewport" name="viewport" content="width=device-width, user scalable=no">
+    <title>InstaClone - Registro</title>
+    <link rel="stylesheet" href="css/style.css"/>
+<link href="https://fonts.googleapis.com/css?family=Courgette|Open+Sans:600" rel="stylesheet">
+  </head>
+  <body>
+    <main>
+      <section id="mainSection">
+        <header>
+          <div class="icone-cam">
+          <img src="images/photo-camera.png" alt="Imagem da Camera">  
+          </div>
+          <div class="instaclone">
+          <span class="insta">Insta</span><span class="clone">Clone</span>  
+          </div>
+        <br>
+          <span class="frase-cadastro">Cadastre-se para ver fotos e vídeos dos seus amigos</span>
+          </header>
+        <div >
+          <form>
+            <div>
+              <input type="text" name="email" placeholder="Seu e-mail">
+            </div>
+            <div>
+              <input type="text" name="nome" placeholder="Nome completo">
+            </div>
+            <div>
+              <input type="text" name="usuario" placeholder="Nome de Usuário">
+            </div>
+            <div>
+              <input type="password" name="senha" placeholder="Senha">
+            </div>
+            <div class="termos">
+              <input type="checkbox" name="concordo" class="check">
+              <label for="concordo" class="concordar">Concordo com os termos,
+                Política de Dados e Política de Cookies.</label>
+            </div>
+            <div class="div-button">
+              <input type="submit" value="Cadastre-se" class="button"> 
+            </div>
+          </form>
+        </div>
+        <div class="faixa">
+          
+        </div>
+        <footer>
+
+           <p class="frase-footer">Já tem uma conta?</p>
+           <a href="login.php"><button>Conecte-se</button></a>
+        </footer>
+      </section>
+    </main>
+  </body>
+</html>
---
+++ login.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="pt" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta id="viewport" name="viewport" content="width=device-width, user scalable=no">
+    <title>InstaClone - Registro</title>
+    <link rel="stylesheet" href="css/style.css"/>
+<link href="https://fonts.googleapis.com/css?family=Courgette|Open+Sans:600" rel="stylesheet">
+  </head>
+  <body>
+    <main>
+      
+      <section id="mainLogin">
+        <header>
+          <div class="icone-cam">
+          <img src="images/photo-camera.png" alt="Imagem da Camera">  
+          </div>
+          <div class="instaclone">
+          <span class="insta">Insta</span><span class="clone">Clone</span>  
+          </div>
+          <br>
+        </header>
+
+        <div class="center">
+          <form>
+            <div>
+              <input type="text" name="usuario" placeholder="Nome de Usuário ou Email">
+            </div>
+            <div>
+              <input type="password" name="senha" placeholder="Senha">
+            </div>
+            <div class="div-button">
+              <input type="submit" value="Entrar" class="button"> 
+            </div>
+          </form>
+        </div>
+        <div class="faixa">
+          
+        </div>
+        <footer>
+
+           <p class="footer-login">Ainda não tem uma conta?</p>
+           <a href="cadastro.php"><button class="button-login">Cadastre-se</button></a>
+        </footer>
+      </section>
+    </main>
+  </body>
+</html>
